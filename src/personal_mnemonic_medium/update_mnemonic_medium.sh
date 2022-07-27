76badd9ba6106fe236aa2c0a00b6659fd6bf0529
ryqiem <ryqiem@gmail.com>
2022-07-26 22:10:18 -0700

    Add Anki launch and sync

--- src/personal_mnemonic_medium/update_mnemonic_medium.sh
+++ src/personal_mnemonic_medium/update_mnemonic_medium.sh
@@ -6,4 +6,18 @@ cd $PROJECT
 
 source .venv/bin/activate
 
+# If Anki.app is not already running, launch it
+if [ ! -n "$(ps -A | grep Anki)" ]; then
+    open -a Anki
+    sleep 3
+fi
+
 python application/main.py -r $HOME"/Library/Mobile Documents/iCloud~md~obsidian/Documents/Life Lessons/" -p "$PROJECT/Life.apkg"
+
+# Send "y" keypress to Anki.app to start sync
+osascript -e 'tell application "Anki" to activate' 
+
+# Send the letter y as keycode to Anki.app
+osascript -e 'tell application "System Events" to keystroke "y"'
+
+
