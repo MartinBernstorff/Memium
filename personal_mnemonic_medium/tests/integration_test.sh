532d883567cbbf47465a61c19ef572d650181237
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-18 18:13:46 +0100

    ci: fix integration tests with new path (#200)
    Auto-created

--- personal_mnemonic_medium/tests/integration_test.sh
+++ personal_mnemonic_medium/tests/integration_test.sh
@@ -1,3 +1,4 @@
+set -e
 docker build . -t personal-mnemonic-medium:latest -f Dockerfile
 docker volume create ankidecks
 
@@ -12,7 +13,7 @@ docker run -itd \
   -v $OUTPUT_DIR:/output \
   --restart unless-stopped \
   personal-mnemonic-medium \
-  python application/main.py /input/ $OUTPUT_DIR \
+  python personal_mnemonic_medium/cli.py /input/ $OUTPUT_DIR \
   --watch \
   --no-use-anki-connect
 
