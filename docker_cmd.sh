cc03bcbaefe8313b522d8ab6198b210b51744bf8
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-18 16:33:14 +0000

    misc.

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -3,7 +3,7 @@ docker build . -t personal-mnemonic-medium:latest -f Dockerfile
 docker volume create ankidecks
 
 docker run -itd \
-  -v $HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Life\ Lessons\ iCloud/:/input \
+  -v /Users/Shared/life-lessons/docs/life-lessons:/input \
   -v $HOME/ankidecks:/output \
   --restart unless-stopped \
   personal-mnemonic-medium \
