58f38bff6394fd33d23887995d600fdd949eaa88
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-19 10:40:34 +0000

    refactor: change prompts into protocols
    Fixes #226

--- personal_mnemonic_medium/domain/prompt_extractors/prompt.py
+++ personal_mnemonic_medium/domain/prompt_extractors/prompt.py
@@ -1,17 +1,22 @@
+from typing import Protocol
+
 from personal_mnemonic_medium.data_access.document_ingesters.document import (
     Document,
 )
 
 
-class Prompt:
+class Prompt(Protocol):
+    note_uuid: str
+    source_note: Document
+    tags: list[str] | None
+    line_nr: int | None
+
     def __init__(
         self,
+        content: str,
         note_uuid: str,
         source_note: Document,
         tags: list[str] | None = None,
         line_nr: int | None = None,
     ):
-        self.tags = tags
-        self.note_uuid = note_uuid
-        self.source_note = source_note
-        self.line_nr = line_nr
+        ...
