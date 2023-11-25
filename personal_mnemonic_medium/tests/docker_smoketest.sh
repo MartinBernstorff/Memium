c1bd1d0b4fc484ba41e689b52b14ee47a264d411
Martin Bernstorff <martinbernstorff@gmail.com>
2023-11-25 10:38:34 +0000

    misc.

--- personal_mnemonic_medium/tests/docker_smoketest.sh
+++ personal_mnemonic_medium/tests/docker_smoketest.sh
@@ -10,6 +10,9 @@ ANKICONNECT_SYNC_APKG=/output/deck.apkg
 mkdir -p $INPUT_DIR
 echo -e "Q. Question here\nA. Answer!" >> $HOME/input/test.md
 
+# Delete the output file if it already exists, to ensure that we are testing the latest version
+rm -f $HOST_OUTPUT_FILE
+
 docker run -i \
   -v $INPUT_DIR:/input \
   -v $HOST_OUTPUT_DIR:/output \
@@ -26,20 +29,6 @@ echo "Checking if file exists at $HOST_OUTPUT_FILE"
 
 if [ -f "$HOST_OUTPUT_FILE" ]; then
     echo "File exists."
-    
-    FILE_TIMESTAMP=$(stat -c %Y "$HOST_OUTPUT_FILE")
-    CURRENT_TIMESTAMP=$(date +%s)
-
-    # Calculate the age of the file by subtracting file's timestamp from current timestamp
-    FILE_AGE=$(($CURRENT_TIMESTAMP - $FILE_TIMESTAMP))
-
-    # Check if file age is less than 1 minute (60 seconds)
-    if [ $FILE_AGE -lt 60 ]; then
-        echo "File age is less than 1 minute."
-    else
-        echo "File age is more than 1 minute." >&2
-        exit 1
-    fi
 else
     echo "File does not exist." >&2
     exit 1
