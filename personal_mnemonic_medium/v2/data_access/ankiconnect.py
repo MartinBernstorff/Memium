ad5f85e1ded6f1893942d3e458480cb9617162a4
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-04 14:38:33 +0000

    misc.

--- personal_mnemonic_medium/v2/data_access/ankiconnect.py
+++ personal_mnemonic_medium/v2/data_access/ankiconnect.py
@@ -128,14 +128,6 @@ class AnkiConnectGateway:
             raise Exception(
                 "response has an unexpected number of fields"
             )
-        if "error" not in response:
-            raise Exception(
-                "response is missing required error field"
-            )
-        if "result" not in response:
-            raise Exception(
-                "response is missing required result field"
-            )
         if response["error"] is not None:
             raise Exception(response["error"])
         return response["result"]
