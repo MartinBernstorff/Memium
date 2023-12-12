37f3dd4542f4fe530491d1e72266a16633abf49c
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-12 16:28:59 +0000

    misc.

--- personal_mnemonic_medium/v2/domain/diff_determiner/test_diff_determiner.py
+++ personal_mnemonic_medium/v2/domain/diff_determiner/test_diff_determiner.py
@@ -17,9 +17,9 @@ def test_diff_determiner():
     assert syncer.only_in_destination() == ["3"]
 
 
-def test_prompt_diff_determiner(tmpdir: Path):
+def test_prompt_diff_determiner(tmp_path: Path):
     syncer = PromptDiffDeterminer(
-        tmp_read_dir=tmpdir, tmp_write_dir=tmpdir
+        tmp_read_dir=tmp_path, tmp_write_dir=tmp_path
     )
 
     source_prompts = [
@@ -39,7 +39,7 @@ def test_prompt_diff_determiner(tmpdir: Path):
         DeletePrompts([QAPrompt(question="c", answer="c")]),
         PushPrompts(
             [QAPrompt(question="a", answer="a")],
-            tmp_write_dir=tmpdir,
-            tmp_read_dir=tmpdir,
+            tmp_write_dir=tmp_path,
+            tmp_read_dir=tmp_path,
         ),
     ]
