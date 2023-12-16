510eb330886fcb0d4fa191e345f4206b06ef844b
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-16 20:59:35 +0100

    feat: use v2 when running docker smoketest
    Fixes #331

--- personal_mnemonic_medium/v2/presentation/cli.py
+++ personal_mnemonic_medium/v2/presentation/cli.py
@@ -10,21 +10,32 @@ from personal_mnemonic_medium.v2.sync_deck import sync_deck
 
 log = logging.getLogger(__name__)
 
+app = typer.Typer()
 
-def main(
+
+@app.command()
+def sync(
     input_dir: Annotated[
         Path,
         typer.Option(
-            help="Directory containing markdown prompts to sync to Anki."
+            help="Directory containing markdown prompts to sync to Anki.",
+            dir_okay=True,
+            file_okay=False,
+            exists=True,
+            readable=True,
         ),
     ],
-    apkg_output_filepath: Annotated[
+    apkg_output_dir: Annotated[
         Path,
         typer.Option(
-            help="Directory to use for temporary storage of .apkg while syncing."
+            help="Directory to use for temporary storage of .apkg while syncing.",
+            dir_okay=True,
+            file_okay=False,
+            exists=True,
+            writable=True,
         ),
     ],
-    host_ankiconnect_dir: Annotated[
+    host_apkg_dir: Annotated[
         Path,
         typer.Option(
             help="Directory in which AnkiConnect will look for the .apkg. Can be different from apkg_output_filepath in cases where the script is running in a Docker container. If None, defaults to apkg_output_filepath"
@@ -39,83 +50,66 @@ def main(
     deck_name: Annotated[
         str,
         typer.Option(
-            default="Personal Mnemonic Medium",
-            help="Anki path to deck, e.g. 'Parent deck::Child deck'",
+            help="Anki path to deck, e.g. 'Parent deck::Child deck'"
         ),
-    ],
+    ] = "Personal Mnemonic Medium",
     max_deletions_per_run: Annotated[
         int,
         typer.Option(
-            default=50,
-            help="Maximum number of cards to delete per sync to avoid unintentional deletions. If exceeded, raises error.",
+            help="Maximum number of cards to delete per sync to avoid unintentional deletions. If exceeded, raises error."
         ),
-    ],
-    log_file: Annotated[
-        Path,
-        typer.Option(
-            default=None,
-            help="File to log to. If None, log to stdout.",
-        ),
-    ],
+    ] = 50,
     dry_run: Annotated[
         bool,
         typer.Option(
-            default=False,
-            help="Don't update via AnkiConnect, just log what would happen",
+            help="Don't update via AnkiConnect, just log what would happen"
         ),
-    ],
+    ] = False,
+    skip_sync: Annotated[
+        bool,
+        typer.Option(help="Skip all syncing, useful for smoketest"),
+    ] = False,
 ):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
         datefmt="%Y/&m/%d %H:%M:%S",
-        filename=log_file,
     )
 
     # Use apkg_output_filepath if host_ankiconnect_dir is not set
-    host_ankiconnect_dir = (
-        host_ankiconnect_dir
-        if host_ankiconnect_dir
-        else apkg_output_filepath
+    host_apkg_dir = (
+        host_apkg_dir if host_apkg_dir else apkg_output_dir
     )
-
-    if not input_dir.exists():
-        raise FileNotFoundError(
-            f"Input directory {input_dir} does not exist"
-        )
-
-    if not apkg_output_filepath.parent.exists():
-        apkg_output_filepath.parent.mkdir(exist_ok=True, parents=True)
-        log.info(f"Creating output directory {host_ankiconnect_dir}")
 
     sentry_sdk.init(
         dsn="https://37f17d6aa7742424652663a04154e032@o4506053997166592.ingest.sentry.io/4506053999984640",
         environment=get_env(default="None"),
     )
 
-    sync_deck(
-        base_deck=deck_name,
-        input_dir=input_dir,
-        apkg_output_dir=apkg_output_filepath,
-        ankiconnect_read_apkg_from_dir=host_ankiconnect_dir,
-        max_deletions_per_run=max_deletions_per_run,
-        dry_run=dry_run,
-    )
-
-    if watch:
-        sleep_seconds = 60
-        log.info(
-            f"Sync complete, sleeping for {sleep_seconds} seconds"
-        )
+    if not skip_sync:
         sync_deck(
             base_deck=deck_name,
             input_dir=input_dir,
-            apkg_output_dir=apkg_output_filepath,
-            ankiconnect_read_apkg_from_dir=host_ankiconnect_dir,
+            apkg_output_dir=apkg_output_dir,
+            ankiconnect_read_apkg_from_dir=host_apkg_dir,
             max_deletions_per_run=max_deletions_per_run,
             dry_run=dry_run,
         )
 
+        if watch:
+            sleep_seconds = 60
+            log.info(
+                f"Sync complete, sleeping for {sleep_seconds} seconds"
+            )
+            sync_deck(
+                base_deck=deck_name,
+                input_dir=input_dir,
+                apkg_output_dir=apkg_output_dir,
+                ankiconnect_read_apkg_from_dir=host_apkg_dir,
+                max_deletions_per_run=max_deletions_per_run,
+                dry_run=dry_run,
+            )
+
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
