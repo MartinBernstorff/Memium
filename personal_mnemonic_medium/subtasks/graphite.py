fc56d67ed1eb5ba0c4daa8f2f120a3431b11fb4c
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-23 13:38:26 +0000

    dev: prettier graphite commit messages
    Fixes #429

--- personal_mnemonic_medium/subtasks/graphite.py
+++ personal_mnemonic_medium/subtasks/graphite.py
@@ -15,5 +15,5 @@ def create_branch_from_issue(c: inv.Context, selected_issue: GithubIssue):
     branch_title = f"{selected_issue.number}-{sanitised_title}"
     c.run(f"gt create {branch_title}")
     c.run(
-        f"git commit --allow-empty -m '{selected_issue.title}. Fixes #{selected_issue.number}'"
+        f"git commit --allow-empty -m $'{selected_issue.title}.\n\nFixes #{selected_issue.number}'"
     )
