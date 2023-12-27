155036f274c0838e7f164a8c7eb86f11b2cf6952
Martin Bernstorff <martinbernstorff@gmail.com>
2023-12-27 14:28:04 +0000

    misc.

--- memium/__main__.py
+++ memium/__main__.py
@@ -1,6 +1,11 @@
+import logging
+from functools import partial
 from pathlib import Path
+from typing import Annotated, Optional
 
-from .cli import app
+import sentry_sdk
+import typer
+
 from .destination.ankiconnect.anki_converter import AnkiPromptConverter
 from .destination.ankiconnect.ankiconnect_gateway import (
     ANKICONNECT_URL,
@@ -10,13 +15,15 @@ from .destination.destination import PushPrompts
 from .destination.destination_ankiconnect import AnkiConnectDestination
 from .destination.destination_dryrun import DryRunDestination
 from .diff_determiner import PromptDiffDeterminer
-from .environment import host_input_dir, in_docker
+from .environment import get_env, host_input_dir, in_docker
 from .source.document_source import MarkdownDocumentSource
 from .source.extractors.extractor_cloze import ClozePromptExtractor
 from .source.extractors.extractor_qa import QAPromptExtractor
 from .source.source import DocumentPromptSource
 
-app()
+log = logging.getLogger(__name__)
+
+app = typer.Typer()
 
 
 def main(
@@ -60,3 +67,82 @@ def main(
         destination.update(commands=update_commands)
     else:
         destination.update([PushPrompts(prompts=source_prompts)])
+
+
+@app.command()
+def cli(
+    input_dir: Annotated[
+        Path,
+        typer.Option(
+            help="Directory containing markdown prompts to sync to Anki.",
+            dir_okay=True,
+            file_okay=False,
+            exists=True,
+            readable=True,
+            writable=True,
+        ),
+    ],
+    watch: Annotated[
+        Optional[int],  # noqa: UP007
+        typer.Option(help="Keep running, updating Anki deck every 15 seconds"),
+    ] = None,
+    deck_name: Annotated[
+        str,
+        typer.Option(help="Anki path to deck, e.g. 'Parent deck::Child deck'"),
+    ] = "Memium",
+    max_deletions_per_run: Annotated[
+        int,
+        typer.Option(
+            help="Maximum number of cards to delete per sync to avoid unintentional deletions. If exceeded, raises error."
+        ),
+    ] = 50,
+    push_all: Annotated[
+        bool,
+        typer.Option(
+            help="Push all prompts to Anki, whether or not they exist in Anki. Useful if you have e.g. changed your CSS template. Note that this does not change scheduling, nor delete prompts that no longer exist in your markdown."
+        ),
+    ] = False,
+    dry_run: Annotated[
+        bool,
+        typer.Option(
+            help="Don't update via AnkiConnect, just log what would happen"
+        ),
+    ] = False,
+    sentry_dsn: Annotated[
+        Optional[str],  # noqa: UP007
+        typer.Option(
+            help="Sentry DSN for error reporting. If not provided, no error reporting will be done."
+        ),
+    ] = None,
+    skip_sync: Annotated[
+        bool, typer.Option(help="Skip all syncing, useful for smoketest")
+    ] = False,
+):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y/&m/%d %H:%M:%S",
+    )
+
+    if sentry_dsn:
+        sentry_sdk.init(dsn=sentry_dsn, environment=get_env(default="None"))
+
+    main_fn = partial(
+        main,
+        base_deck=deck_name,
+        input_dir=input_dir,
+        max_deletions_per_run=max_deletions_per_run,
+        dry_run=dry_run,
+        push_all=push_all,
+    )
+
+    if not skip_sync:
+        main_fn()
+        if watch:
+            sleep_seconds = 60
+            log.info(f"Sync complete, sleeping for {sleep_seconds} seconds")
+            main_fn()
+
+
+if __name__ == "__main__":
+    app()
