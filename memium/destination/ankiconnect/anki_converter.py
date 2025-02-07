dd177bb1dba10565ce0987900dc0303244b01110
Martin Bernstorff <martinbernstorff@gmail.com>
2025-02-07 12:13:52 +0100

    update anki_converter.py

--- memium/destination/ankiconnect/anki_converter.py
+++ memium/destination/ankiconnect/anki_converter.py
@@ -8,7 +8,7 @@ from .ankiconnect_gateway import NoteInfo
 
 
 def front_from_rephrased(prompt: RephrasedQAFromDoc) -> str:
-    return f"""{prompt.rephrased_question} (LLM-rephrased)
+    return f"""{prompt.rephrased_question} <span style="opacity: 0.1">(LLM-rephrased)</span>
 <p style="display: none;">{prompt.question}</p>
 """
 
