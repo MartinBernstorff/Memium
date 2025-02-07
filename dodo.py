d3829cf764867fe68e0ab610e5f5b5cf92f6bda3
Martin Bernstorff <martinbernstorff@gmail.com>
2025-02-07 20:16:52 +0100

    update dodo.py

--- dodo.py
+++ dodo.py
@@ -8,22 +8,22 @@ code_files = (
 
 
 def task_types():
-    return {"actions": ["uv run pyright memium"], "file_dep": code_files, "verbosity": 2}
+    return {"actions": ["uv run pyright memium"], "file_dep": code_files}
 
 
 def task_test():
-    return {"actions": ["uv run pytest memium"], "file_dep": code_files, "verbosity": 2}
+    return {"actions": ["uv run pytest memium"], "file_dep": code_files}
 
 
 def task_lint():
-    return {"actions": ["ruff check memium --fix"], "file_dep": code_files, "verbosity": 2}
+    return {"actions": ["ruff check memium --fix"], "file_dep": code_files}
 
 
 def task_validate():
     return {
         "actions": ["echo ✅✅✅ All OK ✅✅✅"],
         "task_dep": ["lint", "types", "test"],
-        "verbosity": 2,
+        "verbosity": 2,  # Otherwise, output is only displayed in case of error
     }
 
 
@@ -33,7 +33,6 @@ dockerfiles = list(Path().rglob("**/Dockerfile*"))
 def task_image():
     return {
         "actions": ["docker build . -t ghcr.io/martinbernstorff/memium:latest -f Dockerfile"],
-        "verbosity": 2,
         "file_dep": code_files + dockerfiles,
     }
 
@@ -43,9 +42,8 @@ def task_run_image():
         "actions": ["docker compose -f ~/Git/dotfiles/containers/memium/docker-compose.yml up -d"],
         "task_dep": ["image", "validate"],
         "file_dep": code_files + dockerfiles,
-        "verbosity": 2,
     }
 
 
 def task_deploy():
-    return {"actions": None, "task_dep": ["run_image"], "verbosity": 2}
+    return {"actions": None, "task_dep": ["run_image"]}
